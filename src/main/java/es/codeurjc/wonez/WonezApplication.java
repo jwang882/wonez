c72928b3cc167c280de6e22dacb02e8e6cc336a7
v3ruxz <alejandrosmsound@gmail.com>
2024-03-02 17:12:27 +0100

    add

---
+++ src/main/java/es/codeurjc/wonez/WonezApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.wonez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/codeurjc/wonez/WonezApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.wonez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
